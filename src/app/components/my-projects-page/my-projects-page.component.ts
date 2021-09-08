e67402890bef4f1f8b26041115172afda1c38048
Joshuapmb <joshuabradley99@gmail.com>
2021-09-08 21:56:31 +0100

    Populated the projects page
    needs indepth detail

--- src/app/components/my-projects-page/my-projects-page.component.ts
+++ src/app/components/my-projects-page/my-projects-page.component.ts
@@ -10,6 +10,28 @@ export class MyProjectsPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        //@ts-ignore
+        this.classList.toggle("active");
+        //@ts-ignore
+        var content = this.nextElementSibling;
+        //@ts-ignore
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+    
   }
+
+  
+
+
 
 }
